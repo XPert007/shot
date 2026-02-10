255b6e326328c3e8ecb03ee8eaf4eb09818a2550
XPert007 <cookingcapybara@proton.me>
2026-02-10 18:54:06 +0530

    the ball moves

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,37 @@ use macroquad::prelude::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let mut circle_x = screen_width() - 30.0;
-    let circle_y = screen_height() - 30.0;
-    let speed = -2.0;
+    let mut circle_x = screen_width() / 2.0;
+    let mut circle_y = 0.0;
+    let speed = 2.0;
+    let radius = 18.0;
 
     loop {
         clear_background(RED);
 
-        circle_x += speed;
+        draw_line(
+            0.0,
+            screen_height() - radius,
+            screen_width(),
+            screen_height() - radius,
+            3.0,
+            BLACK,
+        );
+        let ground_y = screen_height() - radius;
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(circle_x, circle_y, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 100.0, 100.0, DARKGRAY);
-
+        if circle_y + radius >= ground_y {
+            circle_y = ground_y - radius;
+        } else {
+            circle_y += speed;
+        }
+        draw_circle(circle_x, circle_y, radius, YELLOW);
+        if is_key_down(KeyCode::D) {
+            circle_x += speed;
+        } else if is_key_down(KeyCode::A) {
+            circle_x -= speed;
+        } else if is_key_down(KeyCode::W) {
+            circle_y -= speed * 2.0;
+        }
         next_frame().await;
     }
 }
-
