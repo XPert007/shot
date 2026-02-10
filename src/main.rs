f73c0f6e1853db907ec28947ead9b2725742a7d8
XPert007 <cookingcapybara@proton.me>
2026-02-10 16:21:17 +0530

    writing everything from scratch, we made a moving circle

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,22 @@
-use rapier2d::prelude::*;
+use macroquad::prelude::*;
 
-fn main() {
-    let mut rigid_body_set = RigidBodySet::new();
-    let mut collider_set = ColliderSet::new();
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let mut circle_x = screen_width() - 30.0;
+    let circle_y = screen_height() - 30.0;
+    let speed = -2.0;
 
-    /* Create the ground. */
-    let collider = ColliderBuilder::cuboid(100.0, 0.1).build();
-    collider_set.insert(collider);
+    loop {
+        clear_background(RED);
 
-    /* Create the bouncing ball. */
-    let rigid_body = RigidBodyBuilder::dynamic()
-        .translation(vector![0.0, 10.0])
-        .build();
-    let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
-    let ball_body_handle = rigid_body_set.insert(rigid_body);
-    collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
+        circle_x += speed;
 
-    /* Create other structures necessary for the simulation. */
-    let gravity = vector![0.0, -9.81];
-    let integration_parameters = IntegrationParameters::default();
-    let mut physics_pipeline = PhysicsPipeline::new();
-    let mut island_manager = IslandManager::new();
-    let mut broad_phase = DefaultBroadPhase::new();
-    let mut narrow_phase = NarrowPhase::new();
-    let mut impulse_joint_set = ImpulseJointSet::new();
-    let mut multibody_joint_set = MultibodyJointSet::new();
-    let mut ccd_solver = CCDSolver::new();
-    let physics_hooks = ();
-    let event_handler = ();
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(circle_x, circle_y, 15.0, YELLOW);
+        draw_text("HELLO", 20.0, 100.0, 100.0, DARKGRAY);
 
-    /* Run the game loop, stepping the simulation once per frame. */
-    for _ in 0..200 {
-        physics_pipeline.step(
-            &gravity,
-            &integration_parameters,
-            &mut island_manager,
-            &mut broad_phase,
-            &mut narrow_phase,
-            &mut rigid_body_set,
-            &mut collider_set,
-            &mut impulse_joint_set,
-            &mut multibody_joint_set,
-            &mut ccd_solver,
-            &physics_hooks,
-            &event_handler,
-        );
-
-        let ball_body = &rigid_body_set[ball_body_handle];
-        println!("Ball altitude: {}", ball_body.translation().y);
+        next_frame().await;
     }
 }
+
