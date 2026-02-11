13d312b3fdb670d3c10af5c67a99bf0181464c90
XPert007 <cookingcapybara@proton.me>
2026-02-11 12:47:37 +0530

    added rapier dependance and collision on ball

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,90 @@
 use macroquad::prelude::*;
+use rapier2d::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+const M_TO_PX: f32 = 50.0;
+
+#[macroquad::main("Rapier + Macroquad")]
 async fn main() {
-    let mut circle_x = screen_width() / 2.0;
-    let mut circle_y = 0.0;
-    let speed = 2.0;
-    let radius = 18.0;
+    let gravity = vector![0.0, 9.81];
+    let integration_parameters = IntegrationParameters::default();
+
+    let mut physics_pipeline = PhysicsPipeline::new();
+    let mut island_manager = IslandManager::new();
+    let mut broad_phase = DefaultBroadPhase::new();
+    let mut narrow_phase = NarrowPhase::new();
+    let mut impulse_joint_set = ImpulseJointSet::new();
+    let mut multibody_joint_set = MultibodyJointSet::new();
+    let mut ccd_solver = CCDSolver::new();
+    let physics_hooks = ();
+    let event_handler = ();
+
+    let mut rigid_body_set = RigidBodySet::new();
+    let mut collider_set = ColliderSet::new();
+
+    let ground_body = RigidBodyBuilder::fixed()
+        .translation(vector![
+            screen_width() / 2.0 / M_TO_PX,
+            (screen_height() - 40.0) / M_TO_PX,
+        ])
+        .build();
+
+    let ground_handle = rigid_body_set.insert(ground_body);
+
+    let ground_collider = ColliderBuilder::cuboid(screen_width() / M_TO_PX, 10.0 / M_TO_PX).build();
+
+    collider_set.insert_with_parent(ground_collider, ground_handle, &mut rigid_body_set);
+
+    let radius_m = 0.4;
+
+    let ball_body = RigidBodyBuilder::dynamic()
+        .translation(vector![screen_width() / 2.0 / M_TO_PX, 1.0])
+        .build();
+
+    let ball_handle = rigid_body_set.insert(ball_body);
+
+    let ball_collider = ColliderBuilder::ball(radius_m).restitution(0.7).build();
+
+    collider_set.insert_with_parent(ball_collider, ball_handle, &mut rigid_body_set);
 
     loop {
         clear_background(RED);
 
-        draw_line(
-            0.0,
-            screen_height() - radius,
-            screen_width(),
-            screen_height() - radius,
-            3.0,
-            BLACK,
-        );
-        let ground_y = screen_height() - radius;
+        {
+            let ball = rigid_body_set.get_mut(ball_handle).unwrap();
 
-        if circle_y + radius >= ground_y {
-            circle_y = ground_y - radius;
-        } else {
-            circle_y += speed;
+            if is_key_down(KeyCode::D) {
+                ball.set_linvel(vector![5.0, ball.linvel().y], true);
+            }
+            if is_key_down(KeyCode::A) {
+                ball.set_linvel(vector![-5.0, ball.linvel().y], true);
+            }
+            if is_key_pressed(KeyCode::W) {
+                ball.apply_impulse(vector![0.0, -6.0], true);
+            }
         }
-        draw_circle(circle_x, circle_y, radius, YELLOW);
-        if is_key_down(KeyCode::D) {
-            circle_x += speed;
-        } else if is_key_down(KeyCode::A) {
-            circle_x -= speed;
-        } else if is_key_down(KeyCode::W) {
-            circle_y -= speed * 2.0;
-        }
+
+        physics_pipeline.step(
+            &gravity,
+            &integration_parameters,
+            &mut island_manager,
+            &mut broad_phase,
+            &mut narrow_phase,
+            &mut rigid_body_set,
+            &mut collider_set,
+            &mut impulse_joint_set,
+            &mut multibody_joint_set,
+            &mut ccd_solver,
+            &physics_hooks,
+            &event_handler,
+        );
+
+        let ball = &rigid_body_set[ball_handle];
+        let pos = ball.translation();
+
+        draw_circle(pos.x * M_TO_PX, pos.y * M_TO_PX, radius_m * M_TO_PX, YELLOW);
+
+        draw_rectangle(0.0, screen_height() - 40.0, screen_width(), 20.0, BLACK);
+
         next_frame().await;
     }
 }
