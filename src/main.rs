db9b78feb5a6f14eb1f5b497bb207cf6fdb2da5c
XPert007 <cookingcapybara@proton.me>
2026-02-19 01:21:49 +0530

    restart

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("Basic 3D Ball")]
+#[macroquad::main("Physics")]
 async fn main() {
-    let mut ball_pos = vec3(0.0, 0.0, 0.0);
-    let radius = 1.0;
-
     loop {
-        clear_background(WHITE);
+        clear_background(RED);
 
-        set_camera(&Camera3D {
-            position: vec3(5.0, 10.0, 10.0),
-            up: vec3(0.0, 1.0, 0.0),
-            target: ball_pos,
-            ..Default::default()
-        });
+        draw_line(
+            0.0,
+            screen_height() - 20.0,
+            screen_width(),
+            screen_height() - 20.0,
+            15.0,
+            BLUE,
+        );
 
-        draw_sphere(ball_pos, radius, None, RED);
-
-        draw_grid(20, 1.0, BLACK, GRAY);
-
-        set_default_camera();
-
-        draw_text("Basic 3D Ball", 20.0, 40.0, 30.0, BLACK);
-
-        next_frame().await;
+        next_frame().await
     }
 }
